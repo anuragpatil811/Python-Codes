4b548748958d39b0909bc77308fa7c8d32e6e2c2
Anurag Arun patil <anuragpatil811@gmail.com>
2026-07-17 23:10:02 +0530

    Add files via upload

---
+++ Retrieve-Elements-ThreeDim-Array.py
@@ -0,0 +1,13 @@
+#Program to retrieve elements from three dimensional array
+from numpy import * 
+a = [
+    [[1, 2, 3], 
+     [4, 5, 6]], 
+     [[7, 8, 9], 
+      [10, 11, 12]]]
+for i in range(len(a)):
+    for j in range(len(a[i])):
+        for k in range(len(a[i][j])):
+            print(a[i][j][k], end='\t')
+        print()
+    print()
